d9c8b6f2d4ee1b7fea30066e9f307dc28309430d
Stefano Angaran <stefano.angaran@upyou.it>
2020-10-06 17:20:57 +0200

    Send message on bus when creating a user

---
+++ src/Message/UserMessage.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Oniric85\UsersService\Message;
+
+class UserMessage
+{
+    private string $userId;
+
+    public function __construct(string $userId)
+    {
+        $this->userId = $userId;
+    }
+
+    public function getUserId(): string
+    {
+        return $this->userId;
+    }
+}
--- src/Service/Domain/UserService.php
+++ src/Service/Domain/UserService.php
@@ -4,24 +4,32 @@ namespace Oniric85\UsersService\Service\Domain;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Oniric85\UsersService\Exception\Application\NotFromSwitzerlandException;
+use Oniric85\UsersService\Message\UserMessage;
 use Oniric85\UsersService\Service\Infrastructure\IpApiClient;
 use Oniric85\UsersService\Entity\User;
 use Oniric85\UsersService\Exception\Application\EmailAlreadyUsedException;
 use Oniric85\UsersService\Repository\UserRepository;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class UserService
 {
     private UserRepository $repository;
     private EntityManagerInterface $em;
     private IpApiClient $ipApiClient;
+    private MessageBusInterface $bus;
 
     private const SWITZERLAND_COUNTRY_CODE = 'CH';
 
-    public function __construct(UserRepository $repository, EntityManagerInterface $em, IpApiClient $ipApiClient)
-    {
+    public function __construct(
+        UserRepository $repository,
+        EntityManagerInterface $em,
+        IpApiClient $ipApiClient,
+        MessageBusInterface $bus
+    ) {
         $this->repository = $repository;
         $this->em = $em;
         $this->ipApiClient = $ipApiClient;
+        $this->bus = $bus;
     }
 
     public function createUser(string $email, string $plainTextPassword, string $firstName, string $ip): User
@@ -40,6 +48,8 @@ class UserService
 
         $this->em->persist($user);
         $this->em->flush();
+
+        $this->bus->dispatch(new UserMessage($user->getId()->toString()));
 
         return $user;
     }
