ddcb2bcad35e1c8833471a538d99305c540de5b0
Stefano Angaran <stefano.angaran@upyou.it>
2020-10-06 19:09:21 +0200

    Improved functional test

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class UserControllerTest extends WebTestCase
 {
-    public function testUserCreation()
+    public function testUserCreationIsSuccessful()
     {
         $client = static::createClient();
 
@@ -27,5 +27,118 @@ class UserControllerTest extends WebTestCase
         );
 
         $this->assertResponseStatusCodeSame(Response::HTTP_CREATED);
+        $this->assertJson($client->getResponse()->getContent());
+    }
+
+    public function testUserCreationDoesNotAllowEmailReuse()
+    {
+        $client = static::createClient();
+
+        $data = [
+            'email' => 'test@example.com',
+            'first_name' => 'foobar',
+            'password' => 'password',
+        ];
+
+        $client->request(
+            'POST',
+            '/users',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($data)
+        );
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_CREATED);
+
+        $client->request(
+            'POST',
+            '/users',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($data)
+        );
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_BAD_REQUEST);
+        $this->assertJson($client->getResponse()->getContent());
+    }
+
+    public function testUserUpdateIsSuccessful()
+    {
+        $client = static::createClient();
+
+        $data = [
+            'email' => 'test@example.com',
+            'first_name' => 'foobar',
+            'password' => 'password',
+        ];
+
+        $client->request(
+            'POST',
+            '/users',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($data)
+        );
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_CREATED);
+        $this->assertJson($client->getResponse()->getContent());
+
+        $result = json_decode($client->getResponse()->getContent(), true);
+
+        $updatedData = [
+            'first_name' => 'test',
+        ];
+
+        $client->request(
+            'PUT',
+            sprintf('/users/%s', $result['id']),
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($updatedData)
+        );
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $this->assertJson($client->getResponse()->getContent());
+
+        $result = json_decode($client->getResponse()->getContent(), true);
+        $this->assertSame('test', $result['first_name']);
+    }
+
+    public function testUserSearchByEmail()
+    {
+        $client = static::createClient();
+
+        $data = [
+            'email' => 'test@example.com',
+            'first_name' => 'foobar',
+            'password' => 'password',
+        ];
+
+        $client->request(
+            'POST',
+            '/users',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($data)
+        );
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_CREATED);
+
+        $client->request(
+            'GET',
+            '/users?email=test@example.com'
+        );
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $this->assertJson($client->getResponse()->getContent());
+
+        $result = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertCount(1, $result);
     }
 }
