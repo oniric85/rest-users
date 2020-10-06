b880c11020cf224c67974ff3cae2fa0c2fcb5300
Stefano Angaran <stefano.angaran@upyou.it>
2020-10-06 16:42:37 +0200

    Added SearchUsers request model

---
+++ src/Http/Request/Model/SearchUsers.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Oniric85\UsersService\Http\Request\Model;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class SearchUsers
+{
+    /**
+     * @Assert\Type(
+     *     type="string",
+     *     message="The email must be of type {{ type }}.",
+     *     groups="Strict"
+     * )
+     */
+    private $email;
+
+    /**
+     * @Assert\Type(
+     *     type="string",
+     *     message="The first_name must be of type {{ type }}.",
+     *     groups="Strict"
+     * )
+     */
+    private $first_name;
+
+    public function getFirstName(): ?string
+    {
+        return $this->first_name;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+
+}
